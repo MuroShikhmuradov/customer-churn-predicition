74a29874501b649c481778366e8efa3881a09588
Murad Shixkmuradov <sixmuradovmurad@gmail.com>
2026-06-19 00:47:18 +0300

    Add Logistic Regression

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
+from sklearn.preprocessing import StandardScaler
 
 df = pd.read_csv('data/Telco-Customer-Churn.csv')
 
@@ -26,3 +30,27 @@ df =pd.get_dummies(df, drop_first=True)
 
 X = df.drop('Churn_Yes', axis=1)
 y = df['Churn_Yes']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+scaler = StandardScaler()
+
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+model_lr = LogisticRegression(max_iter=1000)
+model_lr.fit(X_train_scaled, y_train)
+model_lr_prediction = model_lr.predict(X_test_scaled)
+
+accuracy = accuracy_score(y_test, model_lr_prediction)
+print("Accuracy: ", accuracy)
+print("Confusion Matrix: ")
+print(confusion_matrix(y_test, model_lr_prediction))
+print("Classification Report: ")
+print(classification_report(y_test, model_lr_prediction))
+
+
+
+
+
+
